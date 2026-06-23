b7b775546fc319fd347fb789acf76575190e26ec
Ishaan <ishaan.dhanwantry@gmail.com>
2026-06-23 14:39:40 -0700

    code v4

--- projectile-sim.py
+++ projectile-sim.py
@@ -3,17 +3,17 @@ import math
 
 class Particle(object):
     def __init__(self):
-        self.position = [0,0]
+        self.position = [0, 0]
 
 class Projectile(Particle):
-    def __init__(self, mass, velocity, angle, initial_height):
+    def __init__(self, velocity, angle, initial_height, dt):
         super().__init__()
-        self.mass = mass
         self.velocity = velocity
         self.angle = angle
         self.initial_height = initial_height
         self.position = [0, initial_height]
         self.time = 0
+        self.dt = dt
         self.g = -9.81
 
     def update_position(self, dt):
@@ -22,7 +22,7 @@ class Projectile(Particle):
         self.position[1] = self.initial_height + (self.velocity * math.sin(math.radians(self.angle)) * self.time) + (0.5 * self.g * self.time ** 2)
 
     def get_position(self):
-        return self.position
+        return tuple(self.position)
     
     def get_time(self):
         return self.time
@@ -30,9 +30,16 @@ class Projectile(Particle):
     def get_max_height(self):
         return self.initial_height + (self.velocity ** 2 * (math.sin(math.radians(self.angle))) ** 2) / (2 * -self.g)
     
-ball = Projectile(mass=1, velocity=10, angle=45, initial_height=10)
-print(f"initial x, y: {ball.get_position()}(m)")
-while ball.position[1] >= 0:
-    ball.update_position(0.1)
-print(f"x, y: {ball.get_position()}(m),", f"travel time: {ball.get_time()}s,", f"max height: {ball.get_max_height()}m")
+ball = Projectile(velocity=10, angle=45, initial_height=10, dt=0.1)
+print(f"Initial x, y: {ball.get_position()} (m)")
 
+while ball.position[1] >= 0:
+     
+    last_position = ball.get_position()
+    last_time = ball.get_time()
+    
+    ball.update_position(ball.dt)
+
+print(f"Final x, y: ({ball.get_position()[0]:.2f}, {ball.get_position()[1]:.2f}) (m)")
+print(f"Travel time: {ball.get_time():.2f} s")
+print(f"Max height: {ball.get_max_height():.2f} m")
